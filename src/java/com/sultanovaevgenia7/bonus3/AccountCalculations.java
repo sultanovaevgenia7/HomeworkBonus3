037e0d12ca9e9e8d1e6e5dcf23f2d6c175345abd
Evgeniya Sultanova <sultanovaevgenia7@gmail.com>
2022-04-26 12:16:18 -0400

    Move from static into class instance

--- src/java/com/sultanovaevgenia7/bonus3/AccountCalculations.java
+++ src/java/com/sultanovaevgenia7/bonus3/AccountCalculations.java
@@ -1,4 +1,6 @@
-public class Main {
+package com.sultanovaevgenia7.bonus3;
+
+public class AccountCalculations {
     public static final int BONUS_LIMIT = 100;
 
     private static int addReplenishment(int account, int replenishmentAmount) {
@@ -8,7 +10,7 @@ public class Main {
         return (account + replenishmentAmount + bonus);
     }
 
-    public static void main(String[] args) {
+    private void runCalculations(){
         int account = 100;
         System.out.println("Начальный счет " + account + " рублей");
         int replenishmentAmount = 100;
@@ -16,11 +18,14 @@ public class Main {
         account = addReplenishment(account, replenishmentAmount);
         System.out.println("Теперь ваш баланс составляет " + account + " рублей");
 
-        int account2 = 100;
-        System.out.println("Начальный счет " + account2 + " рублей");
         int replenishmentAmount2 = 1100;
         System.out.println("Пополнение " + replenishmentAmount2 + " рублей");
-        account2 = addReplenishment(account2, replenishmentAmount2);
-        System.out.println("Теперь ваш баланс составляет " + account2 + " рублей");
+        account = addReplenishment(account, replenishmentAmount2);
+        System.out.println("Теперь ваш баланс составляет " + account + " рублей");
+    }
+
+    public static void main(String[] args) {
+        AccountCalculations acc = new AccountCalculations();
+        acc.runCalculations();
     }
 }
